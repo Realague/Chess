ebc40ec3ff139e344778cdd27e5a3820d7f9acd3
Realgue <realague@gmail.com>
2020-10-06 15:27:22 +0200

    WIP

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static bool isPlayerTurn = true;
+
+    private bool isPlayerStarting = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!isPlayerStarting)
+        {
+            isPlayerTurn = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayerTurn)
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/PawnScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class PawnScript : MonoBehaviour
+{
+
+    public GameObject highlightedMove;
+
+    public GameObject highlightedMoveKill;
+
+    private bool isFirstMove = true;
+
+    private bool isSelected = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        isSelected = true;
+        GameObject obj1 = null;
+        if (isFirstMove)
+        {
+            obj1 = Instantiate(highlightedMove, transform.position + new Vector3(0, 0, 2), new Quaternion(45, 0, 0, 45));
+        }
+        GameObject obj = Instantiate(highlightedMove, transform.position + new Vector3(0, 0, 1), new Quaternion(45, 0, 0, 45));
+
+        /*if (isFirstMove)
+        {
+            Destroy(obj1);
+        }
+        Destroy(obj);
+        isFirstMove = false;*/
+    }
+
+}
